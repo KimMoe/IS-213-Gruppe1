9cb3a0fdcecd7d3ffb2d0bdc4fbfedb4e94c4179
Sakseid <vegar@sakseid.no>
2018-04-27 10:56:57 +0200

    Normalizer class

--- PongNNet/src/NeuralNetwork/NN.java
+++ PongNNet/src/NeuralNetwork/NN.java
@@ -3,65 +3,61 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package NeuralNetwork;
 
-//import java.io.FileNotFoundException;
-//import java.io.IOException;
-//import org.neuroph.core.NeuralNetwork;
-//import org.neuroph.core.data.DataSet;
-//import org.neuroph.nnet.Perceptron;
-//import org.neuroph.nnet.learning.MomentumBackpropagation;
-//import org.neuroph.util.TrainingSetImport;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.neuroph.core.NeuralNetwork;
+import org.neuroph.core.data.DataSet;
+import org.neuroph.nnet.Perceptron;
+import org.neuroph.nnet.learning.MomentumBackpropagation;
+import org.neuroph.util.TrainingSetImport;
+import org.neuroph.util.data.norm.Normalizer;
 
 /**
  *
  * @author vegar
  */
-public class NN {
-/*
-    COMMENTED OUT BECAUSE IT WASEN'T WORKING. AND I DON'T KNOW HOW IT WORKS.
-*/    
+public class NN implements Normalizer{   
+    private NeuralNetwork neuralNetwork;
+    private MomentumBackpropagation learningRule;
+    private DataSet trainingSet;
+    private int inputCount;
+    private int outputCount;
+    private String fileName;
+
+    public NN() throws IOException {
+        inputCount = 3; //Position pad, ball and score
+        outputCount= 1; //If the pad should go up or down
+        fileName = "data.txt";
+        neuralNetwork = new Perceptron(inputCount,outputCount); //Input: paddle, ball x, ball y, (poeng?)
+        learningRule = (MomentumBackpropagation) neuralNetwork.getLearningRule();
+        learningRule.setLearningRate(0.5); //vet ikke om disse verdiene er korrekte
+        learningRule.setMomentum(0.8);
+        trainingSet();
+    }
+ 
+    /**
+     * Mulig løsning; skrive data fra et spill til en fil, NN lærer datasettet før neste spill
+     */
+    private void trainNN(){
+        neuralNetwork.learn(trainingSet);
+        
+        
+    }
     
-    
-//    private NeuralNetwork neuralNetwork;
-//    private MomentumBackpropagation learningRule;
-//    private DataSet trainingSet;
-//    private int inputCount;
-//    private int outputCount;
-//    private String fileName;
-//
-//    public NN() {
-//        inputCount = 3;
-//        outputCount= 1;
-//        fileName = "data.txt";
-//        neuralNetwork = new Perceptron(inputCount,outputCount); //Input: paddle, ball x, ball y, (poeng?)
-//        learningRule = (MomentumBackpropagation) neuralNetwork.getLearningRule();
-//        learningRule.setLearningRate(0.5); //vet ikke om disse verdiene er korrekte
-//        learningRule.setMomentum(0.8);
-//        trainingSet();
-//    }
-// 
-//    /**
-//     * Mulig lÃ¸sning; skrive data fra et spill til en fil, NN lÃ¦rer datasettet fÃ¸r neste spill
-//     */
-//    private void trainNN(){
-//        neuralNetwork.learn(trainingSet);
-//    }
-//    
-//    private void trainingSet(){
-//        try{
-//            trainingSet = TrainingSetImport.importFromFile(fileName, inputCount, outputCount, ",");
-//        }
-//        catch (FileNotFoundException ex){
-//            System.out.println("File " + fileName + " not found");
-//        }
-//        catch (IOException|NumberFormatException ex) {
-//            System.out.println("Error reading file or bad number format!");
-//        }
-//    }
+    private void trainingSet() throws IOException{
+        try{
+            trainingSet = TrainingSetImport.importFromFile(fileName, inputCount, outputCount, ",");
+        }
+        catch (NumberFormatException ex) {
+            System.out.println("Error reading file or bad number format!");
+        }
+    }
+
+    @Override
+    public void normalize(DataSet dataSet) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
